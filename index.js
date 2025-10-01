8e449ac27e8c9d6d6ef297d7617f8769e2e8d073
{jezz995} <{jezz.veranda995@gmail.com}>
2025-10-01 23:02:16 +0700

    add README and edit index.js

--- index.js
+++ index.js
@@ -13,39 +13,50 @@ const ai = new GoogleGenAI({});
 //inisialisasi middleware
 
 app.use(cors());
-app.use(multer());
+//app.use(multer());
+app.use(express.json());
 
 //inisialisasi endpoint
 
-app.post('/chat',async (req, res) => {
-    const { body } = req;
-    const { prompt} = body;
+app.post("/chat", async (req, res) => {
+  const { body } = req;
+  const { prompt } = body;
 
-    //guard clause
-    if (!prompt || typeof prompt !== 'string'){
-        res.status(400).json({
-            message: 'prompt harus diisi dan berupa string!',
-            data: null,
-            success: false
-        });
-        return;
-    }
+  //guard clause
+  if (!prompt || typeof prompt !== "string") {
+    res.status(400).json({
+      message: "prompt harus diisi dan berupa string!",
+      data: null,
+      success: false,
+    });
+    return;
+  }
+  //main part part penting
+  try {
+    const aiResponse = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: [
+        {
+          parts: [{ text: prompt }],
+        },
+      ],
+    });
 
-    try { 
-            const aiResponse = await ai.models.generateContent({
-                model: 'gemini-2.5-flash',
-                contents: [ { parts: }
-
-                ]            })
-                res.status(200).json({
-                    success: true,
-                    message: aiResponse.text,
-                    message: 'Berhasil di tanggapi oleh google gemini'
-                });
-    } catch(e){}
+    res.status(200).json({
+      success: true,
+      message: aiResponse.text,
+      message: "Berhasil di tanggapi oleh google gemini",
+    });
+  } catch (e) {
     console.log(e);
     res.status(500).json({
-        success: false,
-        message:
-    })
-};
+      success: false,
+      message: e.message || "Ada masalah di server!!",
+    });
+  }
+});
+
+//buat entry point
+app.listen(3000, () => {
+  console.log("server jalan di 3000");
+});
