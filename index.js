a41316c0aaa758c35858bd138e5b7250328666b9
{jezz995} <{jezz.veranda995@gmail.com}>
2025-10-01 22:21:42 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//proses dependensi
+import express from "express";
+import cors from "cors";
+import { GoogleGenAI } from "@google/genai";
+
+import "dotenv/config";
+
+//inisialisasi express
+
+const app = express();
+const ai = new GoogleGenAI({});
+
+//inisialisasi middleware
+
+app.use(cors());
+app.use(multer());
+
+//inisialisasi endpoint
+
+app.post('/chat',async (req, res) => {
+    const { body } = req;
+    const { prompt} = body;
+
+    //guard clause
+    if (!prompt || typeof prompt !== 'string'){
+        res.status(400).json({
+            message: 'prompt harus diisi dan berupa string!',
+            data: null,
+            success: false
+        });
+        return;
+    }
+
+    try { 
+            const aiResponse = await ai.models.generateContent({
+                model: 'gemini-2.5-flash',
+                contents: [ { parts: }
+
+                ]            })
+                res.status(200).json({
+                    success: true,
+                    message: aiResponse.text,
+                    message: 'Berhasil di tanggapi oleh google gemini'
+                });
+    } catch(e){}
+    console.log(e);
+    res.status(500).json({
+        success: false,
+        message:
+    })
+};
